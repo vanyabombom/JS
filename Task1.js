ea683944f61855410c54686846fdc8a8fe51541c
vanyabombom <vanyadondov@gmail.com>
2024-12-04 13:53:35 +0200

    2/10

---
+++ Task1.js
@@ -0,0 +1,17 @@
+let age = prompt("Enter your age");
+if(age <= 2)
+{
+    alert("Child");
+}
+else if(age > 12 && age < 18)
+{
+    alert("Teen");
+}
+else if(age > 18 && age < 60)
+{
+    alert("Adult");
+}
+else if(age > 60)
+{
+    alert("Dedulya");
+}
---
+++ Task2.js
@@ -0,0 +1,34 @@
+let symbol = prompt("Enter number (0 - 9)")
+if (number == 0) {
+    alert(`)`);
+}
+else if (number == 1) {
+    alert(`!`);
+}
+else if (number == 2) {
+    alert(`@`);
+}
+else if (number == 3) {
+    alert(`#`);
+}
+else if(number == 4){
+    alert(`$`);
+}
+else if(number == 5){
+    alert(`%`);
+}
+else if(number == 6){
+    alert(`^`);
+}
+else if(number == 7){
+    alert(`&`);
+}
+else if(number == 8){
+    alert(`*`);
+}
+else if(number == 9){
+    alert(`(`);
+}
+else{
+    alert("Error");
+}
